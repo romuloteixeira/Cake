12278bbf9a02320ef074191f7f4deb11bab7c00d
TSIBSPROC\romulo.teixeira <romulo.teixeira@sibs.com>
2021-04-13 12:00:24 +0100

    Add project files.

---
+++ Cake.Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Cake.Calculator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
